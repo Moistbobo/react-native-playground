f5fc258a6d21f0b8d5586d1fb4aa14ad7c1b0489
Moistbobo <moistbobo@outlook.com>
2021-04-14 14:28:28 +0800

    chore: detox setup

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,3 +1,5 @@
+import {device, element, by} from 'detox';
+
 describe('Example', () => {
   beforeAll(async () => {
     await device.launchApp();
@@ -11,13 +13,13 @@ describe('Example', () => {
     await expect(element(by.id('welcome'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
+  // it('should show hello screen after tap', async () => {
+  //   await element(by.id('hello_button')).tap();
+  //   await expect(element(by.text('Hello!!!'))).toBeVisible();
+  // });
+  //
+  // it('should show world screen after tap', async () => {
+  //   await element(by.id('world_button')).tap();
+  //   await expect(element(by.text('World!!!'))).toBeVisible();
+  // });
 });
